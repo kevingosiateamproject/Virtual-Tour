c82fcda4a4fb7582ca77de4412ccb41d60ddaefd
Kevin1995 <kevinkehoe18@gmail.com>
2017-01-19 15:57:22 +0000

    Movement
    Player controller has been added to move the vector3 using the input
    keys

---
+++ PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMovement movement;
+
+    void Start()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        // Calculate movement as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _yMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov; // (1, 0, 0)
+        Vector3 _movVertical = transform.forward * _yMov; // (0, 0, 1)
+
+        // Final Movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        // Apply Movement
+        movement.Move(_velocity);
+    }
+}
