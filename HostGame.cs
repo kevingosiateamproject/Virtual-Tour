e5cd5911374f966402472653425ce042fbffe19c
Kevin1995 <kevinkehoe18@gmail.com>
2017-02-26 14:31:54 +0000

    Adding the host script
    This allows users to use the UI to create a room with ease

---
+++ HostGame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+    [SerializeField]
+    private uint roomSize = 12;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom()
+    {
+        // Here we are making sure that the user is hosting a room with a name so other people can see the room name
+        // When looking to join a room
+        if(roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating room: " + roomName + " with room for " + roomSize + "players.");
+            // Create Room
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+
+        }
+    }
+}
