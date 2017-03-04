aaf773e93f308c8c01e09189bdbc254fa36f3a4c
Kevin1995 <kevinkehoe18@gmail.com>
2017-03-04 14:41:54 +0000

    JoinGame

---
+++ JoinGame.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame: MonoBehaviour
+{
+
+    List<GameObject> roomList = new List<GameObject>();
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    // Making a reference to our Network Manager
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+    }
+
+    // Calling a public void so that we can access the UI elements
+    public void RefreshRoomList()
+    {
+        ClearRoomList(); // I switched this here because i want to clear the rooms before i refresh
+        networkManager.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
+        status.text = "Loading....";
+    }
+
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        status.text = "";
+
+        if (matchList == null)
+        {
+            status.text = "Couldn't get room list.";
+            return;
+        }
+
+        foreach (MatchInfoSnapshot match in matchList)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+
+            RoomListItem _roomListItem = _roomListItemGO.GetComponent<RoomListItem>();
+            if(_roomListItem != null)
+            {
+                _roomListItem.Setup(match, JoinRoom);
+            }
+            // that will take care of setting up the name/amount of users
+            // as well as setting up a callback function that will join the game.
+
+            roomList.Add(_roomListItemGO);
+        }
+
+        // if we have no rooms to join we give a new text value
+        if(roomList.Count == 0)
+        {
+            status.text = "No rooms at the moment..";
+        }
+    }
+
+
+    void ClearRoomList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        // We have to do this because the for loop will destroy the object but not the reference to the object
+        roomList.Clear();
+    }
+
+    public void JoinRoom(MatchInfoSnapshot _match)
+    {
+        Debug.Log("Joining " + _match.name);
+        networkManager.matchMaker.JoinMatch(_match.networkId, "", "", "", 0, 0, networkManager.OnMatchJoined);
+        ClearRoomList();
+        status.text = "JOINING...";
+    }
+
+}
---
+++ RoomListItem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking.Match;
+
+public class RoomListItem : MonoBehaviour {
+
+    // A delegate is a type that represents references to methods with a particular parameter list and return type.
+    public delegate void JoinRoomDelegate(MatchInfoSnapshot _room);
+    // Instance of JoinRoomDelegate
+    private JoinRoomDelegate joinRoomCallback;
+
+    [SerializeField]
+    private Text roomNameText;
+
+    private MatchInfoSnapshot room;
+
+    public void Setup(MatchInfoSnapshot _room, JoinRoomDelegate _joinRoomCallback)
+    {
+        room = _room;
+        joinRoomCallback = _joinRoomCallback;
+
+        roomNameText.text = room.name + " (" + room.currentSize + "/" + room.maxSize + ")";
+    }
+
+    public void JoinRoom()
+    {
+        joinRoomCallback.Invoke(room);
+    }
+}
